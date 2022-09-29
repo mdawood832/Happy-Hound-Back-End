5af408182ec4474eab62a4cb79880e270c8c0a96
knorth <kayci.north@gmail.com>
2022-09-28 18:17:47 -0600

    cleaned up code

--- config/db.connection.js
+++ config/db.connection.js
@@ -16,21 +16,6 @@ mongoose.connection.on('error', (error) => console.log('mongodb error', error));
 
 mongoose.connection.on('disconnected', () => console.log('mongodb disconnected 💩'));
 
-// import mongoose from 'mongoose'
 
-// const connectionStr =
-//   process.env.MONGODB_URI 
-  
-// mongoose.set('returnOriginal', false)
-
-// mongoose
-//   .connect(connectionStr)
-//   .catch((error) => console.error('Error connecting to MongoDB:', error.message))
-  
-// mongoose.connection.on('disconnected', () => console.log(`Disconnected from ${connectionStr}`))
-
-// mongoose.connection.on('error', (error) => console.error(`MongoDB connection error: ${error}`))
-
-// export default mongoose.connection
 
 
--- models/Product.js
+++ models/Product.js
@@ -29,32 +29,3 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema)
 module.exports = Product
 
-// import mongoose from 'mongoose'
-// const Schema = mongoose.Schema
-
-// const Product = new Schema(
-//   {
-//     name: { 
-//       type: String, 
-//       required: true 
-//       },
-//     imgURL: { 
-//       type: String, 
-//       required: true 
-//       },
-//     description: { 
-//       type: String, 
-//       required: true 
-//       },
-//     type: { 
-//       type: String, 
-//       required: false  
-//       },
-//     price: { 
-//       type: String, 
-//       required: true 
-//       } 
-//   }
-// )
-
-// export default mongoose.model('products', Product)
