130bc2c86a77fd3838b351d80a97c38e2d52b304
Eric-Peltzman33 <erpeltz@gmail.com>
2022-10-05 18:52:29 -0400

    comments on user route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 /* == CTRLS == */
 const ctrls = require('../controllers');
 
-router.post('/signup', ctrls.users.signup);
-router.post('/login', ctrls.users.login);
-router.delete('/logout', ctrls.users.logout);
+router.post('/signup', ctrls.users.signup); // /signup route to signup a new user
+
+router.post('/login', ctrls.users.login); // /login route to login a user 
+
+ router.delete('/logout', ctrls.users.logout); 
 
 
 module.exports = router;
